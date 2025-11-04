060b6532b41e93a9b162e04a2bd82a377dcc63a3
Gerson Borja <hipersocial08@gmail.com>
2025-11-04 00:09:03 -0600

    cree tres modelos base segun el enunciado y agrege mi propia plantilla para usar tailwind en las vistas

---
+++ FASE II DAS Gestion de expedientes CRUD/Models/Alumno.cs
@@ -0,0 +1,20 @@
+﻿namespace FASE_II_DAS_Gestion_de_expedientes_CRUD.Models
+{
+    public class Alumno
+    {
+        int AlumnoId { get; set; }
+        string Nombre  { get; set; }
+        string Apellido { get; set; }
+        DateTime FechaNacimiento { get; set; }
+        int Grado { get; set; }
+
+        public Alumno(int alumnoId, string nombre, string apellido, DateTime fechaNacimiento, int grado)
+        {
+            AlumnoId = alumnoId;
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNacimiento = fechaNacimiento;
+            Grado = grado;
+        }
+    }
+}
---
+++ FASE II DAS Gestion de expedientes CRUD/Models/Expediente.cs
@@ -0,0 +1,21 @@
+﻿namespace FASE_II_DAS_Gestion_de_expedientes_CRUD.Models
+{
+    public class Expediente
+    {
+        int ExpedienteId { get; set; }
+        int AlumnoId { get; set; }
+        int MateriaId { get; set; }
+        int NotaFinal { get; set; }
+        string Observaciones { get; set; }
+        public Expediente(int expedienteId, int alumnoId, int materiaId, int notaFinal, string observaciones)
+        {
+            ExpedienteId = expedienteId;
+            AlumnoId = alumnoId;
+            MateriaId = materiaId;
+            NotaFinal = notaFinal;
+            Observaciones = observaciones;
+        }
+
+
+    }
+}
