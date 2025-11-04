d0251b924aa63d697b35b995c686fb75bab4cd8e
Gerson Borja <hipersocial08@gmail.com>
2025-11-03 23:48:35 -0600

    Agregar archivos de proyecto.

---
+++ FASE II DAS Gestion de expedientes CRUD/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using FASE_II_DAS_Gestion_de_expedientes_CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FASE_II_DAS_Gestion_de_expedientes_CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FASE II DAS Gestion de expedientes CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FASE_II_DAS_Gestion_de_expedientes_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
