6284e687448d830b27bfe399e805901565e304a2
Gerson Borja <hipersocial08@gmail.com>
2025-11-07 02:07:36 -0600

    se agrego la vista editar y eliminar en ambos tambien se configuro el controlador segun su tipo ya sea get para mostrar o post para procesamiento de formulario

--- FASE II DAS Gestion de expedientes CRUD/Controllers/ExpedienteController.cs
+++ FASE II DAS Gestion de expedientes CRUD/Controllers/ExpedienteController.cs
@@ -24,5 +24,61 @@ namespace FASE_II_DAS_Gestion_de_expedientes_CRUD.Controllers
 
             return View(alumno);
         }
+
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var expediente = _context.Expedientes
+                .Include(e => e.Alumno)
+                .FirstOrDefault(e => e.ExpedienteId == id);
+
+            if (expediente == null)
+                return NotFound();
+
+            return View(expediente);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Expediente expediente)
+        {
+            var existente = _context.Expedientes.Find(expediente.ExpedienteId);
+            if (existente == null)
+                return NotFound();
+
+            existente.NotaFinal = expediente.NotaFinal;
+            existente.Observaciones = expediente.Observaciones;
+            _context.SaveChanges();
+
+            return RedirectToAction("Ver", new { id = expediente.AlumnoId });
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            var expediente = _context.Expedientes
+                .Include(e => e.Alumno)
+                .Include(e => e.Material)
+                .FirstOrDefault(e => e.ExpedienteId == id);
+
+            if (expediente == null)
+                return NotFound();
+
+            return View(expediente);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int expedienteId, int alumnoId)
+        {
+            var expediente = _context.Expedientes.Find(expedienteId);
+            if (expediente == null)
+                return NotFound();
+
+            _context.Expedientes.Remove(expediente);
+            _context.SaveChanges();
+
+            return RedirectToAction("Ver", new { id = alumnoId });
+        }
+
     }
 }
